0062bd05446614a3af0ddf778268a614bb624bae
Ruyssen Romain <ruyssensud@gmail.com>
2023-11-20 23:08:44 +0100

    first commit

---
+++ JS/script.js
@@ -0,0 +1,61 @@
+// @ts-nocheck
+        document.addEventListener('DOMContentLoaded', () => {
+            const input = document.getElementById('input_task');
+            const addButton = document.getElementById('add_button');
+            const todos = document.querySelector('.todos');
+            const filters = document.getElementsByClassName('filter');
+            
+            
+            // ajouter une tache 
+            function addTodo () {
+              let taskText = input.value.trim();
+              if (taskText === "") {
+                window.alert(`veillez ajouter une nouvelle tache \n
+                avant de valider`)
+                return
+              }
+
+            //  creer un nouvel element li
+              let newTodo = document.createElement('li');
+              newTodo.textContent = taskText;
+              todos.appendChild(newTodo);
+              console.log(newTodo); // RETIRER POUR PROD
+
+            //effcacer le champ du texte 
+            input.value = "";
+            }
+
+            // ajout au click d une tache avec le button 
+            addButton.addEventListener('click', addTodo);
+
+
+            // ecouteur lors de la touche relaché === ENTRER 
+            input.addEventListener('keyup', (event) => {
+                if ( event.key === 'Enter') {
+                    addTodo()
+                }
+            })
+        })
+
+
+
+
+
+// POUR APPLIQUER UNE FONCTION DE RECHERCHE DANS LA TO DO LIST  
+// Élément input pour la recherche dans la to-do list
+// var searchInput = document.getElementById('searchTasks');
+
+// // Écouteur d'événement pour la recherche en temps réel
+// searchInput.addEventListener('keyup', function(event) {
+//   var searchText = searchInput.value.toLowerCase();
+//   var tasks = document.querySelectorAll('#tasks-list li');
+
+//   tasks.forEach(function(task) {
+//     var taskText = task.textContent.toLowerCase();
+//     if (taskText.includes(searchText)) {
+//       task.style.display = ''; // La tâche correspond, on l'affiche
+//     } else {
+//       task.style.display = 'none'; // La tâche ne correspond pas, on la cache
+//     }
+//   });
+// });
